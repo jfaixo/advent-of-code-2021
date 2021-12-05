f6e59c54acd27bc86e5c028632daef8b560f80db
Julien Faixo <julien@faixo.me>
2021-12-05 20:34:22 +0100

    :sparkles: day-2

--- workspace/day-5/src/main.rs
+++ workspace/day-5/src/main.rs
@@ -6,6 +6,7 @@ use crate::input_parsing::parse_file;
 mod models;
 mod input_parsing;
 mod part_1;
+mod part_2;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
@@ -17,7 +18,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let input  = parse_file(args[1].clone())?;
 
     println!("part 1: {}", part_1::overlapping_points_count(&input));
-    // println!("part 2: {}", part_2::find_last_winning_board(&input));
+    println!("part 2: {}", part_2::overlapping_points_count(&input));
 
     Ok(())
 }
